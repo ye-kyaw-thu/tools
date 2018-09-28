652455d1fb0b5db2e7036391a4972416b0fad194
Ye Kyaw Thu <wasedakuma@gmail.com>
2018-09-29 02:34:12 +0900

    Update print-emojiSentences.pl

--- perl/print-emojiSentences.pl
+++ perl/print-emojiSentences.pl
@@ -1,11 +1,13 @@
 #!/usr/bin/env perl
 
-# printing sentences that contained emoji character
+# printing sentences that contain emoji character
+# with -r option, the program will print sentences that do not contain emoji character
 # Ye Kyaw Thu, NICT, Kyoto, Japan
 #
 # last updated: 25 April 2015
 # usage: perl print-emojiSentences.pl <input-file>
 # e.g. perl ./print-emojiSentences.pl ./tst-emoji 
+# e.g. perl ./print-emojiSentences.pl ./tst-emoji r
 
 use strict;
 use warnings;
@@ -17,15 +19,35 @@ binmode(STDERR, ":utf8");
 
 open (my $inputFILE,"<:encoding(utf8)", $ARGV[0]) or die "Couldn't open input file $ARGV[0]!, $!\n";
 
-while (!eof($inputFILE)) {
+if (! defined $ARGV[1]){
+ 
+   while (!eof($inputFILE)) {
      
-    my $line = <$inputFILE>;
-    if (($line ne '') & ($line !~ /^ *$/) & ($line =~ /(\p{Emoticons})/g)) {
-        chomp($line);
+      my $line = <$inputFILE>;
+      if (($line ne '') & ($line !~ /^ *$/) & ($line =~ /(\p{Emoticons})/g)) {
+         chomp($line);
          
         print "$line\n";
+       }
+
+    }
+  
+}
+elsif ($ARGV[1] eq "r"){
+
+   while (!eof($inputFILE)) {
+     
+      my $line = <$inputFILE>;
+      if (($line ne '') & ($line !~ /^ *$/) & ($line !~ /(\p{Emoticons})/g)) {
+         chomp($line);
+         
+        print "$line\n";
+       }
+
     }
 
+
 }
+
 
 close ($inputFILE);
