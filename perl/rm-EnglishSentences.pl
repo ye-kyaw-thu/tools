e810c9f855dd46d2959c5ef4cf6ef011905ff91c
Ye Kyaw Thu <wasedakuma@gmail.com>
2018-09-28 18:35:13 +0900

    Add files via upload

---
+++ perl/rm-EnglishSentences.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+# Remove sentences that contained English character or word
+# Ye Kyaw Thu, Waseda University, Tokyo, Japan
+#
+# usage: perl rm-EnglishSentences.pl <input-file>
+
+use strict;
+use warnings;
+use utf8;
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+
+open (my $inputFILE,"<:encoding(utf8)", $ARGV[0]) or die "Couldn't open input file $ARGV[0]!, $!\n";
+
+while (!eof($inputFILE)) {
+     
+    my $line = <$inputFILE>;
+    if ($line !~  /^\s*$/) {
+        chomp($line);
+
+        #  if you used "$line !~ /\W/", the program will printout one word sentences 
+        if ($line !~ /[a-zA-z0-9]/ ) {
+           print "$line\n";
+         }
+    }
+
+}
+
+close ($inputFILE);
+
