02fed45d0355f04c8cd8db1cc8a93bf4690568e7
Ye Kyaw Thu <wasedakuma@gmail.com>
2019-01-27 12:57:34 +0900

    Update print-CRLF.sh

--- bash/print-CRLF.sh
+++ bash/print-CRLF.sh
@@ -4,4 +4,7 @@
 # How to run: print-CRLF.sh <filename> 
 # Written by Ye Kyaw Thu, Waseda Univ., Tokyo
 
+# $'\xHH' syntax for hex value
+# "-n" option is for printing line number
+# "-U or --binary" option treat the file(s) as binary
 grep -nU $'\x0D' $1
