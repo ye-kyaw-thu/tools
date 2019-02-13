307f89c380ecada5b89557a714bb1667eef6502f
Ye Kyaw Thu <wasedakuma@gmail.com>
2019-02-13 18:57:23 +0700

    Add files via upload

---
+++ bash/rm-stopwords.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# for removing stop words
+# written by Ye, LST Lab., NECTEC, Thailand
+
+stopwordFile=$1;
+corpusFile=$2;
+
+# same with following line. using ":" delimiter instead of using "/" for easier reading 
+# sed -e "$(sed 's/.*/s\/& \/\/ig/' $stopwordFile)" $corpusFile;
+# Note space character after &
+
+sed -e "$(sed 's:.*:s/& //ig:' $stopwordFile)" $corpusFile;
