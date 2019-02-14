c190b127ffc7badcdd01ef9fe4e731aad6667bac
Ye Kyaw Thu <wasedakuma@gmail.com>
2019-02-14 13:06:23 +0900

    Update rm-stopwords.sh

--- bash/rm-stopwords.sh
+++ bash/rm-stopwords.sh
@@ -2,12 +2,20 @@
 
 # for removing stop words
 # written by Ye, LST Lab., NECTEC, Thailand
+# how to run: ./rm-stopwords.sh <stopwords filename> <corpus filename>
+# e.g. ./rm-stopwords.sh ./stopwords.txt ./my-text.txt
 
 stopwordFile=$1;
 corpusFile=$2;
 
-# same with following line. using ":" delimiter instead of using "/" for easier reading 
-# sed -e "$(sed 's/.*/s\/& \/\/ig/' $stopwordFile)" $corpusFile;
-# Note space character after &
+sed 's/^.*$/\<\|&\|\>/' $stopwordFile | sed 's/ /\|\>&\<\|/g' > $stopwordFile.tmp;
+sed 's/^.*$/\<\|&\|\>/' $corpusFile | sed 's/ /\|\>&\<\|/g' > $corpusFile.tmp;
 
-sed -e "$(sed 's:.*:s/^& //ig:' $stopwordFile)" $corpusFile | sed -e "$(sed 's:.*:s/& //ig:' $stopwordFile)" ;
+# print out stop words removed output
+# here, using ":" delimiter instead of using "/" for easier reading
+# sed "s/.*/s\/&\/\/ig/"
+sed -e "$(sed "s:.*:s/&//ig:" $stopwordFile.tmp)" $corpusFile.tmp | sed 's/<|\||>//g' 
+
+# remove temp files
+rm $stopwordFile.tmp;
+rm $corpusFile.tmp;
