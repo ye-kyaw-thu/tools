3a5dd8e2c4da49b578b249b3ec1d728569b8fb23
Ye Kyaw Thu <wasedakuma@gmail.com>
2019-04-02 21:55:42 +0900

    Update replace-with-lineno2.sh

--- bash/replace-with-lineno2.sh
+++ bash/replace-with-lineno2.sh
@@ -11,14 +11,20 @@
 while read line
 do
 
+   # getting line no only and assigned to the variable lineNo
    lineNo=$(echo "${line}" | awk -F$'\t' '{print $1}');
+   
+   # getting original sentence and assigned to the variable originalText 
    originalText=$(echo "${line}" | awk -F$'\t' '{print $2}');
+   
+   # getting edited sentence and assigned to the variable editedText
    editedText=$(echo -e "${line}" | awk -F$'\t' '{print $3}');
 
    echo "lineNo: $lineNo";
    echo "originalText: $originalText";
    echo "editedText: $editedText";
 
+   # sed command for search and replace with line number
    sed -i "$lineNo s/$originalText/$editedText/" "$2";
 
 done < $1
