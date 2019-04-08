b7e079f89f4da96e9e545b0ad0c7d17e02b1f617
Ye Kyaw Thu <wasedakuma@gmail.com>
2019-04-08 22:02:12 +0900

    Update get-words-with-position.sh

--- bash/get-words-with-position.sh
+++ bash/get-words-with-position.sh
@@ -3,12 +3,12 @@
 # retrieving words of a sentence with start and end positions
 # written by Ye Kyaw Thu, Waseda University, Tokyo, Japan
 #
-# How to use: ./get-words-with-position.sh <filename> <position_of_start_word> <position_of_end_word>
+# How to use: ./get-words-with-position.sh <filename> <position_of_start_word> <number_of_words>
 # e.g.1: ./get-words-with-position.sh ./my-text.txt 1 1
 # e.g.2: ./get-words-with-position.sh ./my-text.txt 3 2 
 
 start_word=$2;
-end_word=$3;
+no_of_words=$3;
 
 cat $1 | while read -r line
 do
@@ -18,7 +18,7 @@ do
    set -- $line
 
    # accessing elements of the array "$@" with start and end indexes
-   echo ${@:$start_word:$end_word}
+   echo ${@:$start_word:$no_of_words}
    
 done
 
