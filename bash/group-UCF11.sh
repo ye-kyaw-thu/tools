3130e7f9d63629fbf596db445ca9b3f8bdf634e7
Ye Kyaw Thu <wasedakuma@gmail.com>
2019-06-23 05:25:43 +0700

    Add files via upload

---
+++ bash/group-UCF11.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Grouping .mpg and .xgtf files based on action categories of UCF YouTube Action Data Set
+# Written by Ye, Language Understanding Lab., Pyin Oo Lwin, Myanmar
+# How to run: ./group-UCF11.sh 
+
+#Example filenames of UCF-11 folder:
+#v_walk_dog_25_04.mpg
+#v_walk_dog_25_04.xgtf
+
+for class in biking diving golf juggle jumping riding shooting spiking swing tennis walk_dog;
+do
+
+   mkdir $class;
+   for file in *$class*;
+   do
+      echo "moving $file into class $class ...";
+      mv $file ./$class/;
+   done
+   
+done
