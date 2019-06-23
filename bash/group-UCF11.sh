7020ac09aca390167e25de5155b9a35017d9afe9
Ye Kyaw Thu <wasedakuma@gmail.com>
2019-06-23 19:42:04 +0900

    Update group-UCF11.sh

--- bash/group-UCF11.sh
+++ bash/group-UCF11.sh
@@ -2,11 +2,13 @@
 
 # Grouping .mpg and .xgtf files based on action categories of UCF YouTube Action Data Set
 # Written by Ye, Language Understanding Lab., Pyin Oo Lwin, Myanmar
-# How to run: ./group-UCF11.sh 
+# How to run: ./group-UCF11.sh ./UCF11/
 
 #Example filenames of UCF-11 folder:
 #v_walk_dog_25_04.mpg
 #v_walk_dog_25_04.xgtf
+
+cd $1;
 
 for class in biking diving golf juggle jumping riding shooting spiking swing tennis walk_dog;
 do
@@ -17,3 +19,5 @@ do
       mv $file ./$class/;
    done   
 done
+
+cd ..;
