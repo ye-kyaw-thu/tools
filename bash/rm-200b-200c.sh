26c9d7b3e7037d82fdbacb4d3103d0135a668b7d
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-02-10 14:29:21 +0900

    Add files via upload

---
+++ bash/rm-200b-200c.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# for removing "\u200b" and "\u200d" characters from a Myanmar syllable list
+# after removing, make column layout for easier checking "\u200b" and "\u200d"
+# Written by Ye Kyaw Thu, LST Lab., NECTEC, Thailand
+
+# How to run: ./rm-200b-200d.sh <input-file>
+# e.g. ./rm-200b-200d.sh ./all.manual.uniq
+
+sed -i -e "s/$(echo -ne '\u200b')//g; s/$(echo -ne '\u200d')//g;" $1; 
+column ./all.manual.uniq > ./all.manual.uniq.column
+vi ./all.manual.uniq.column
+
