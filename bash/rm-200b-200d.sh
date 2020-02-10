10174f5ff062aab469529404412cbc510d188246
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-02-10 14:35:55 +0900

    Update rm-200b-200d.sh

--- bash/rm-200b-200d.sh
+++ bash/rm-200b-200d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # for removing "\u200b" and "\u200d" characters from a Myanmar syllable list
-# after removing, make column layout for easier checking "\u200b" and "\u200d"
+# after removing "\u200b" and "\u200d" characters, change to column layout for easier checking "\u200b" and "\u200d"
 # Written by Ye Kyaw Thu, LST Lab., NECTEC, Thailand
 
 # How to run: ./rm-200b-200d.sh <input-file>
