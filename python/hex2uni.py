d5c5c07a479593a4fbdfedfa3e25b3f6474776db
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-03-30 22:51:58 +0900

    Update hex2uni.py

--- python/hex2uni.py
+++ python/hex2uni.py
@@ -5,6 +5,7 @@ import sys
 # Ref: https://gehrcke.de/2014/02/command-line-argument-binary-data/
 # How to run: 
 # e.g. python2.7 ./hex2uni.py $'\xe1\x80\x80\xe1\x80\xae\xe1\x80\x9c\xe1\x80\xba'
+# output: ကီလ်
 
 # this also work!
 print(bytes.decode(b"\xe1\x80\x80\xe1\x80\xae\xe1\x80\x9c\xe1\x80\xba", 'utf-8'))
