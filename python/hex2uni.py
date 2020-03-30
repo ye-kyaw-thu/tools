616cfe2a3f1f01b0d432e4b50f90a395fe634560
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-03-30 20:17:14 +0630

    Add files via upload

---
+++ python/hex2uni.py
@@ -0,0 +1,14 @@
+import sys
+
+# Sometimes, we need to know hex values when you are working with Python2x programs
+# Written by Ye, LST Lab., NECTEC, Thailand
+# Ref: https://gehrcke.de/2014/02/command-line-argument-binary-data/
+# How to run: 
+# e.g. python2.7 ./hex2uni.py $'\xe1\x80\x80\xe1\x80\xae\xe1\x80\x9c\xe1\x80\xba'
+
+# this also work!
+print(bytes.decode(b"\xe1\x80\x80\xe1\x80\xae\xe1\x80\x9c\xe1\x80\xba", 'utf-8'))
+
+for argument in sys.argv[1:]:
+    decoded_argument = argument.decode("utf-8")
+    print (decoded_argument)
