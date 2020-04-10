c4591abcdf89339b8889c83ebec1d127edbfe3a6
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-04-10 21:32:27 +0900

    Update print-char.sh

--- bash/print-char.sh
+++ bash/print-char.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Written by Ye Kyaw Thu, LST Lab., NECTEC, Thailand
 # for printing character by character
+# Written by Ye Kyaw Thu, LST Lab., NECTEC, Thailand
 # How to run: ./print-char.sh <input-filename>
 
 inputFile=$1;
