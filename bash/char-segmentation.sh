19bf489e24d6539b21c4dea93e23699481ed4e1a
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-04-10 21:47:00 +0900

    Update char-segmentation.sh

--- bash/char-segmentation.sh
+++ bash/char-segmentation.sh
@@ -9,6 +9,6 @@
 #
 # ./char-segmentation.sh ./chk.tmp
 # န န ် က ေ ာ ် ဖ ီ သ ေ ာ ့ - က ် ့ ် ့ ် ့ ဟ ှ ိ ု ့ လ ာ း ဆ ိ ု ဟ ှ ီ း င ါ ပ ြ ေ ာ ဇ ာ ။ 
-# By using ./char-segmentation.sh, now you can clearly seen three athat and auk-ka-myint typing mistake
+# By using ./char-segmentation.sh, now you can clearly seen "three athat and auk-ka-myint" typing mistake of a Dawei sentence
 
 sed 's/\(.\)/\1 /g' $1 | sed 's/ \+/ /g';
