817f48a1067d0d5a0b7143b2bbb9a839cb95d9ba
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-06-22 20:47:13 +0630

    Update epitranscribe.py

--- python/epitranscribe.py
+++ python/epitranscribe.py
@@ -5,6 +5,12 @@
 # Ref: https://stackoverflow.com/questions/21689365/python-3-typeerror-must-be-str-not-bytes-with-sys-stdout-write/21689447
 # http://ipa-reader.xyz/
 
+# Updated the original code by Ye Kyaw Thu, LSt Lab., NECTEC, Thailand
+# Used for Ms. Shwe Sin Moe (YTU) Myanmar names TTS project
+
+# Note: you have to install epitran on your computer in advance 
+# How to run: cat ./example.txt | python ../bin/epitranscribe.py "mya-Mymr" 
+
 import sys
 import unicodedata
 import epitran
