4f1468ff80069c33a7a5f699c63154de89b4e3ea
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-06-28 17:04:01 +0630

    Update test.sylbreak.pm.pl

--- perl/test.sylbreak.pm.pl
+++ perl/test.sylbreak.pm.pl
@@ -1,8 +1,14 @@
 #!/usr/bin/perl
 
+# Written by Ye Kyaw Thu, LST, NECTEC, Thailand
+# Example usage of sylbreak perl module
+# How to run:
+# e.g. cat ./bbc-article-27june2020.5sent.txt | perl ./test.sylbreak.pm.pl 
+
 use lib './';
 use sylbreak;
-#require sylbreak;
+# perl module ကို ခေါ်သုံးတဲ့ အခါမှာ use နဲ့ မဟုတ်ပဲ require နဲ့လည်း သုံးနိုင်ပါတယ်
+#require sylbreak; 
 
 use strict;
 use warnings;
