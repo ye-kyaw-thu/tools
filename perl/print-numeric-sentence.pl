f6c8328cbe89b63c407938954f7d03dbe6df49cf
Ye Kyaw Thu <yktnlp@gmail.com>
2020-07-06 16:55:44 +0900

    Add files via upload

---
+++ perl/print-numeric-sentence.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+# Print only sentences that contained Myanmar numbers
+# Ye Kyaw Thu, Waseda University, Tokyo, Japan
+#
+# usage: perl print-numeric-sentence.pl <input-file>
+# Ref: https://www.unicode.org/charts/PDF/U1000.pdf
+
+use strict;
+use warnings;
+use utf8;
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+
+open (my $inputFILE,"<:encoding(utf8)", $ARGV[0]) or die "Couldn't open input file $ARGV[0]!, $!\n";
+
+while (!eof($inputFILE)) {
+     
+    my $line = <$inputFILE>;
+    if ($line !~  /^\s*$/) {
+        chomp($line);
+        if ($line =~ m/[0-9]/){
+           print "$line\n";
+         }
+    }
+
+}
+
+close ($inputFILE);
