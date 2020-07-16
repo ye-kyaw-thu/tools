f6f3899a68440be04002b92c41d2099982064cdd
Ye Kyaw Thu <yktnlp@gmail.com>
2020-07-17 02:52:09 +0900

    Add files via upload

---
+++ bash/rm-heading-tab-lineno.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# for removing heading tab, space and line no 
+# How to run: ./rm-heading-tab-lineno.sh <filename>
+# Written by Ye Kyaw Thu, LST, NECTEC, Thailand 
+
+# using perl one liner
+perl -p -e 's/^[ *|\t][0-9]*\s*//g' $1
