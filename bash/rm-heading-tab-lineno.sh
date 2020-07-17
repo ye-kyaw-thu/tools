11aa3331997e6eda40fe090c0a579600817420fd
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-07-17 11:31:41 +0630

    Update rm-heading-tab-lineno.sh

--- bash/rm-heading-tab-lineno.sh
+++ bash/rm-heading-tab-lineno.sh
@@ -4,5 +4,9 @@
 # How to run: ./rm-heading-tab-lineno.sh <filename>
 # Written by Ye Kyaw Thu, LST, NECTEC, Thailand 
 
+
 # using perl one liner
 perl -p -e 's/^[ *|\t][0-9]*\s*//g' $1
+
+# adjust based on your data format
+#perl -p -e 's/^[ |\t]*[0-9]+[ |\t]*//g' $1
