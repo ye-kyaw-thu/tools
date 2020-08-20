620852c1e3d1c9f572ad04fa33892ee1cebbb950
Ye Kyaw Thu <wasedakuma@gmail.com>
2020-08-20 12:20:29 +0630

    Update my-font-chk.sh

--- bash/my-font-chk.sh
+++ bash/my-font-chk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# listing installed Myanmar language fonts on your Linux machine
+# listing installed Myanmar language font files on your Linux machine
 # written by Ye Kyaw Thu, LST, NECTEC, Thailand
 # Reference: man fc-list
 # How to run: ./my-font-chk.sh
