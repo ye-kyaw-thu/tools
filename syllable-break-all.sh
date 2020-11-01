858a0425eee1ee5763379be3d6b3ebd86f3b1a0a
Ye Kyaw Thu <yktnlp@gmail.com>
2020-11-01 20:40:33 +0900

    Add files via upload

---
+++ syllable-break-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# demo code of syllable breaking of several Myanmar utf8 text files ...
+# written by Ye Kyaw Thu, LST, NECTEC, Thailand
+# how to run:
+# e.g. ./syllable-break-all.sh newg1 newg2 newg3
+
+for FILE in $*;
+do
+   echo "Syllable breaking of $FILE...";
+   perl ./sylbreak.pl -i $FILE -s " " > $FILE.syl
+   perl ./clean-space.pl ./$FILE.syl > $FILE.syl.clean
+   echo "head $FILE.syl.clean";
+   head $FILE.syl.clean
+   echo "=====";
+
+done
