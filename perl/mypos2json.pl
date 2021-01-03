ec1b39504085cb9393b8ee55f6c1aed6c9e072a3
Ye Kyaw Thu <wasedakuma@gmail.com>
2021-01-04 04:44:31 +0630

    Update mypos2json.pl

--- perl/mypos2json.pl
+++ perl/mypos2json.pl
@@ -7,6 +7,7 @@ use utf8;
 # Written by Ye Kyaw Thu, LST, NECTEC, Thailand
 # How to run:
 # e.g. perl ./mypos2json.pl <input-file>
+# e.g. perl ./mypos2json.pl ./mypos-dver.1.0.txt.clean > ./mypos.json
 
 binmode STDIN,  ":utf8";
 binmode STDOUT, ":utf8";
