ca0f9967f3a096e9f260f68e51f040c34a22934a
Ye Kyaw Thu <yktnlp@gmail.com>
2021-01-04 04:21:53 +0630

    Add files via upload

---
+++ perl/mypos2json.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use utf8;
+
+# for mypos data to json format conversion
+# Written by Ye Kyaw Thu, LST, NECTEC, Thailand
+# How to run:
+# e.g. perl ./mypos2json.pl <input-file>
+
+binmode STDIN,  ":utf8";
+binmode STDOUT, ":utf8";
+binmode(STDERR, ":utf8");
+
+open (my $inputFILE,"<:encoding(utf8)", $ARGV[0]) or die "Couldn't open input file $ARGV[0]!, $!\n";
+print("[\n");
+   while(my $line = <$inputFILE>)
+   {
+      if ($line!~/^$/)
+      {
+         chomp ($line);
+         $line =~ s/\|/ /g; # if your data not using pipe "|" comment out this line
+         my @token = split('\s', $line);
+         print("  [\n"); my $tmpStr="";
+         foreach my $one_token(@token)
+         {
+            my($word, $POS) = split('\/', $one_token);
+            $tmpStr=$tmpStr.",\n    [\"$word\"\,\"$POS\"]";
+         }
+         $tmpStr=~s/^,\n//; $tmpStr=$tmpStr."\n  ]";
+        print("$tmpStr\n");
+      }
+    }
+print("]\n");
+
+close($inputFILE);
