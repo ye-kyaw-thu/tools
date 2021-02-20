42bfdbf9a538e4077e7444f2feb8b925bb9d5399
Ye Kyaw Thu <wasedakuma@gmail.com>
2021-02-20 11:03:53 +0630

    Update find-edit-gui.sh

--- bash/find-edit-gui.sh
+++ bash/find-edit-gui.sh
@@ -8,9 +8,9 @@
 reply=1;
 while [ $reply -eq 1 ]; do
 
-   # find command နဲ့ *.txt ဖိုင်တွေကို ရှာပြီး tr command ကို လက်ဆက်ကမ်းမယ်
-   # tr command က ရှာလို့တွေတဲ့ ဖိုင်နာမည်တစ်ခုစီကို စာကြောင်းတစ်ကြောင်းစီ ဖြစ်သွားအောင် space နေရာမှာ \n (newline) နဲ့ အစားထိုးတဲ့ အလုပ်ကို လုပ်တယ်
-   # 
+   # find command နဲ့ *.txt ဖိုင်တွေကို ရှာပြီး tr command ကို လက်ဆက်ကမ်းမယ်  
+   # tr command က ရှာလို့တွေတဲ့ ဖိုင်နာမည်တစ်ခုစီကို စာကြောင်းတစ်ကြောင်းစီ ဖြစ်သွားအောင် space နေရာမှာ \n (newline) နဲ့ အစားထိုးတဲ့ အလုပ်ကို လုပ်တယ်  
+   # ပြီးမှ zenity command ဆီကို လက်ဆင့်ကမ်းတယ်။ zenity ရဲ့ listbox ထဲမှာ ရှာလို့တွေ့တဲ့ text ဖိုင်တွေက တန်းစီပြီး ပေါ်လာအောင် လုပ်ခိုင်းထားတယ်။  
    filePath=$(find  .  -name  '*.txt' | tr ' ' '\n' | zenity --list --title "Search Results" --text "Finding all header files.." --column "Files");
 
    if [ "$filePath" != "" ]
