b4f19f2c23cb029142612d5e4e3d9f825b3dced3
Ye Kyaw Thu <yktnlp@gmail.com>
2021-05-13 15:50:49 +0700

    Add files via upload

---
+++ bash/mk-background-transparent.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# for making an image background transparent...
+# I used this script for preparing a transparent background signature image.
+# written by Ye Kyaw Thu, LST, NECTEC, Thailand
+# last updated: 13 May 2021
+
+# Reference Link:
+# https://legacy.imagemagick.org/discourse-server/viewtopic.php?t=32090
+# https://legacy.imagemagick.org/discourse-server/viewtopic.php?f=1&t=36208
+# https://legacy.imagemagick.org/discourse-server/viewtopic.php?t=18196
+# https://stackoverflow.com/questions/28136471/image-enhancement-solution
+# https://en.wikipedia.org/wiki/SRGB
+
+
+imageFILE=$1;
+
+# convert to gray scale image
+convert $imageFILE -colorspace Gray -auto-level ./gray.png;
+
+# get background color information
+bgColor=`convert ./gray.png -format "%[pixel:u.p{0,0}]" info:`;
+
+# fill background color with white color
+convert ./gray.png -fuzz 50% -fill 'srgb(255,255,255)' -opaque $bgColor ./gray-white.png;
+
+# make transparent background
+convert ./gray-white.png -transparent 'srgb(255,255,255)' -fuzz 30% ./bg-transparent.png;
+
+# check the output
+display $imageFILE & display ./bg-transparent.png;
+
+# clean temp files
+rm ./gray.png ./gray-white.png;
