884eddd63934aeab9c04268485a81e3d6eda9e6e
Ye Kyaw Thu <yktnlp@gmail.com>
2021-07-12 20:25:11 +0700

    Add files via upload

---
+++ python/en-word-tokenizer.py
@@ -0,0 +1,24 @@
+import sys
+import argparse
+from nltk.tokenize import word_tokenize
+
+# word tokenizing for English with NLTK library
+# Written by Ye Kyaw Thu, LST, NECTEC, Thailand
+# Reference: Python 3 Text Processing with NLTK 3 Cookbook
+
+# How to run:
+# python ./en-word-tokenizer.py ./en.sentence.txt
+# cat en.sentence.txt | python ./en-word-tokenizer.py
+# python ./en-word-tokenizer.py < ./en.sentence.txt
+
+parser=argparse.ArgumentParser()
+parser.add_argument('inputFile', default=sys.stdin, type=argparse.FileType('r'), nargs='?')
+
+args=parser.parse_args()
+textLines=args.inputFile.readlines()
+
+count=0
+for line in textLines:
+   count +=1
+   print(word_tokenize(line))
+
