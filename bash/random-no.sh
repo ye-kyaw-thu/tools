bf889a1447ef74eccc9bbe0ac91ddb214f37401f
Ye Kyaw Thu <wasedakuma@gmail.com>
2021-10-22 11:37:19 +0700

    Update random-no.sh

--- bash/random-no.sh
+++ bash/random-no.sh
@@ -3,7 +3,6 @@
 # random number generation with bash
 # written by Ye Kyaw Thu, NECTEC, Thailand
 # 22 Oct 2021
-# Reference: https://www.baeldung.com/linux/random-numbers
 #
 # How to use: ./random-no.sh <min> <max> <no-of-iter>
 # e.g. ./random-no.sh 1 6 10 
