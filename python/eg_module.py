ab12b4bf89fd020af3553b3737cdeb64c0fb1549
Ye Kyaw Thu <yktnlp@gmail.com>
2021-11-04 11:18:54 +0700

    Add files via upload

---
+++ python/eg_module.py
@@ -0,0 +1,14 @@
+"""
+ဒီ module ကို how-name-eq-main-work.py ထဲကနေ import လုပ်ပြီးခေါ်သုံးမှာပါ။
+
+Written by Ye Kyaw Thu
+LST, NECTEC, Thailand
+Date 4 Nov 2021
+"""
+
+print('eg-module.py ထဲကနေ print လုပ်လိုက်တာပါ!')
+
+if __name__ == '__main__':
+    print('eg-module.py ရဲ့ main method ထဲကနေ print လုပ်လိုက်တာပါ ...')
+    
+    
---
+++ python/how-name-eq-main-work.py
@@ -0,0 +1,17 @@
+"""
+if __name__ == '__main__': ရဲ့ အလုပ်လုပ်ပုံကို နားလည်စေဖို့ ဥပမာ ရေးပြထားတဲ့ python code ပါ။
+
+Written by Ye Kyaw Thu
+LST, NECTEC, Thailand
+Date 4 Nov 2021
+
+"""
+
+import eg_module
+
+print('main method ရဲ့ အပြင်မှာပါ ...')
+
+if __name__ == '__main__':
+    print('main method ရဲ့ အထဲမှာပါ ...')
+    
+    
