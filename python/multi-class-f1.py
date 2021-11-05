fcaccc39e28a6000ab59f0f848feb6d48c6a975d
Ye Kyaw Thu <wasedakuma@gmail.com>
2021-11-05 09:14:10 +0700

    Update multi-class-f1.py

--- python/multi-class-f1.py
+++ python/multi-class-f1.py
@@ -12,19 +12,18 @@ from random import randint
 computer_roll = [random.randint(1,6) for x in range(100)]
 print("Dice roll by a model: ", computer_roll)
 
-user1_roll = [random.randint(1,6) for x in range(100)]
-print("Dice roll by user1: ", user1_roll)
+player1_roll = [random.randint(1,6) for x in range(100)]
+print("Dice roll by player1: ", player1_roll)
 
-user2_roll = [random.randint(1,6) for x in range(100)]
-print("Dice roll by user2: ", user2_roll)
+player2_roll = [random.randint(1,6) for x in range(100)]
+print("Dice roll by player2: ", player2_roll)
 
 #calculate multi-class F1 scores
 # important: we need to set the average parameter to "None" to output the per class F1 score.
 # အင်္ဂလိပ်လိုလည်း ရေးထားသလိုပါပဲ multi-class F1 score ကိုတွက်ပေးတာလည်း sklearn.metrics ရဲ့ f1_score function ပါပဲ
 # အရေးကြီးတာက average=None ဆိုတဲ့ parameter ကို ထည့်ပေးရပါမယ်။ မဟုတ်ရင် error ပေးပါလိမ့်မယ်။
-user1_score=f1_score(computer_roll, user1_roll, average=None)
-print("F1 score for user1: ", user1_score)
-user2_score=f1_score(computer_roll, user2_roll, average=None)
-print("F1 score for user2: ", user2_score)
-
+player1_score=f1_score(computer_roll, player1_roll, average=None)
+print("F1-score for player1: ", player1_score)
+player2_score=f1_score(computer_roll, player2_roll, average=None)
+print("F1-score for player2: ", player2_score)
 
