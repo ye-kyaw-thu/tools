5e3dccedbcb08ae2f68e84933ffbccb32732dfbf
Ye Kyaw Thu <yktnlp@gmail.com>
2022-02-07 16:26:05 +0700

    Update sort-capitalized-letter-first.sh

--- bash/sort-capitalized-letter-first.sh
+++ bash/sort-capitalized-letter-first.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Written by Ye, LST Lab., NECTEC, Thailand
+# Written for sorting capitalized letter first and then small letter 
+# Used for mdpi Journal latex template, \abbreviations{Abbreviations}
+# Last updated: 7 Feb 2022
+# How to run:
+# $ ./sort-capitalized-letter-first.sh <input-filename>
 #
 # Ref: https://unix.stackexchange.com/questions/162084/sort-and-ls-why-arent-capitalized-letters-sorted-first/162086
 
