669922d69cf22038b26bf32ad13311d9e1f0035b
Ye Kyaw Thu <yktnlp@gmail.com>
2023-07-02 13:33:08 +0700

    Add files via upload

---
+++ python/dKNN-ver2.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 13:13:10 2023
+
+@author: Ye Kyaw Thu, LST Lab., Myanmar
+
+Reference:
+https://ogunlao.github.io/2020/05/23/knn-as-a-neural-network.html
+
+How to run:
+python dKNN.py --help
+
+python dKNN.py --mode knn --dataset wine
+python dKNN.py --mode diffknn --dataset iris
+python dKNN.py --mode diffknn --dataset wine
+python dKNN.py --mode deepknn --dataset wine --n_layers 2 --epochs 100
+python dKNN.py --mode deepknn --dataset wine --n_layers 6 --epochs 100
+
+For Drawing Dot Diagrams:
+python dKNN-ver2.py --mode deepknn --draw_diagram
+python dKNN-ver2.py --mode diffknn --draw_diagram
+"""
+
+
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from graphviz import Digraph
+
+
+class Net(nn.Module):
+    def __init__(self, input_dim, n_layers):
+        super(Net, self).__init__()
+        layers = []
+        hidden_dim = 16
+        for _ in range(n_layers):
+            layers.append(nn.Linear(input_dim, hidden_dim))
+            layers.append(nn.ReLU())
+            input_dim = hidden_dim
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+class DiffKNN(nn.Module):
+    def __init__(self, n_neighbors):
+        super(DiffKNN, self).__init__()
+        self.n_neighbors = n_neighbors
+
+    def forward(self, X_train, X_test):
+        dists = torch.cdist(X_test, X_train)
+        _, idxs = dists.topk(self.n_neighbors, dim=1, largest=False)
+        return idxs
+
+def load_data(dataset_name):
+    if dataset_name == "iris":
+        data = datasets.load_iris()
+    elif dataset_name == "wine":
+        data = datasets.load_wine()
+
+    X, y = data.data, data.target
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def draw_deepknn():
+    dot = Digraph(format='png')
+    dot.node('I', 'Input Layer\n(Dimension: d)')
+    dot.node('H1', 'Hidden Layer 1\n(Dimension: 16)')
+    dot.node('A1', 'ReLU Activation')
+    dot.node('H2', 'Hidden Layer 2\n(Dimension: 16)')
+    dot.node('A2', 'ReLU Activation')
+    dot.node('K', 'K-Nearest Neighbors (KNN)')
+    dot.edges([('I', 'H1'), ('H1', 'A1'), ('A1', 'H2'), ('H2', 'A2'), ('A2', 'K')])
+    dot.render('deepknn', cleanup=True)
+
+def draw_diffknn():
+    dot = Digraph(format='png')
+    dot.node('I', 'Input Layer\n(Dimension: d)')
+    dot.node('D', 'Pairwise Distance Calculation')
+    dot.node('T', 'Top-k Selection')
+    dot.edges([('I', 'D'), ('D', 'T')])
+    dot.render('diffknn', cleanup=True)
+
+def main(mode, dataset, epochs, n_layers, n_neighbors, draw_diagram):
+    
+    if mode in ["knn", "diffknn"] and epochs != 10:
+        raise ValueError("The 'epochs' argument should not be specified for 'knn' and 'diffknn' modes.")
+    if mode in ["knn", "diffknn"] and n_layers != 2:
+        raise ValueError("The 'n_layers' argument should not be specified for 'knn' and 'diffknn' modes.")
+
+    X_train, X_test, y_train, y_test = load_data(dataset)
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.long)
+
+    if mode == "knn":
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+        knn.fit(X_train, y_train)
+        y_pred = knn.predict(X_test)
+        print(f"Dataset: {dataset}")
+        print(f"Test accuracy (KNN): {accuracy_score(y_test, y_pred)}")
+        print(f"Confusion matrix (KNN):\n {confusion_matrix(y_test, y_pred)}")
+    elif mode == "deepknn":
+        model = Net(X_train.shape[1], n_layers)
+        optimizer = optim.Adam(model.parameters())
+        criterion = nn.CrossEntropyLoss()
+        for epoch in range(epochs):
+            optimizer.zero_grad()
+            outputs = model(X_train)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, y_train)
+            loss.backward()
+            optimizer.step()
+        train_outputs = model(X_train)
+        test_outputs = model(X_test)
+        idxs = DiffKNN(n_neighbors)(train_outputs.detach(), test_outputs)
+        y_pred = y_train[idxs].mode(dim=1)[0].numpy()
+        print(f"Dataset: {dataset}")        
+        print(f"Test accuracy (DeepKNN): {accuracy_score(y_test, y_pred)}")
+        print(f"Confusion matrix (DeepKNN):\n {confusion_matrix(y_test, y_pred)}")
+    elif mode == "diffknn":
+        model = DiffKNN(n_neighbors)
+        idxs = model(X_train, X_test)
+        y_pred = y_train[idxs].mode(dim=1)[0].numpy()
+        print(f"Dataset: {dataset}")
+        print(f"Test accuracy (DiffKNN): {accuracy_score(y_test, y_pred)}")
+        print(f"Confusion matrix (DiffKNN):\n {confusion_matrix(y_test, y_pred)}")
+
+    if draw_diagram:
+        if mode == 'deepknn':
+            draw_deepknn()
+        elif mode == 'diffknn':
+            draw_diffknn()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This program provides 3 methods for classification: KNN, DeepKNN, and DiffKNN. \
+        KNN is the traditional K-Nearest Neighbors. DeepKNN first trains a neural network on the input data, and \
+        uses the trained model to transform the input data before applying the KNN algorithm. DiffKNN treats the \
+        K-Nearest Neighbors process as a differentiable operation, optimizing the entire process end-to-end.')
+    parser.add_argument("--mode", type=str, default="knn", choices=["knn", "deepknn", "diffknn"], help="The mode to run: traditional KNN, DeepKNN, or DiffKNN.")
+    parser.add_argument("--dataset", type=str, default="iris", choices=["iris", "wine"], help="The dataset to use: Iris or Wine.")
+    parser.add_argument("--epochs", type=int, default=10, help="The number of epochs to train for (DeepKNN only).")
+    parser.add_argument("--n_layers", type=int, default=2, help="The number of layers in the neural network (DeepKNN only).")
+    parser.add_argument("--n_neighbors", type=int, default=3, help="The number of neighbors to use in KNN.")
+    parser.add_argument("--draw_diagram", action='store_true', help="Draw and save a diagram of the neural network architecture.")
+    args = parser.parse_args()
+
+    main(args.mode, args.dataset, args.epochs, args.n_layers, args.n_neighbors, args.draw_diagram)
