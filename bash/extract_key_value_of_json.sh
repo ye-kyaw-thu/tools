124121e2c473ce851358410cdbe673491a371a2f
Ye Kyaw Thu <yktnlp@gmail.com>
2024-09-12 16:39:06 +0700

    Update extract_key_value_of_json.sh

--- bash/extract_key_value_of_json.sh
+++ bash/extract_key_value_of_json.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+## Written by Ye, LST, NECTEC, Thailand.  
+## Date: 12 Sept 2024  
+
 jq -r '.Tbl_MMProverbs[] | "\(.ProverbId)|||\(.ProverbName)|||\(.ProverbDesp)"' ./MyanmarProverbs.json > extracted.out
